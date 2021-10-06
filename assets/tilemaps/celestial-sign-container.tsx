0dd6cbf93a0118a89294c6d7be2849d7399a0c79
john-lay <john_lay@hotmail.co.uk>
2021-10-06 22:49:22 +0100

    Redesign quest status screen
    Attempt to make the screens more uniform and inline with source material

--- assets/tilemaps/celestial-sign-container.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="celestial-sign-container" tilewidth="8" tileheight="8" tilecount="36" columns="6">
- <image source="../tilesets/celestial-sign-container.png" width="48" height="48"/>
-</tileset>
