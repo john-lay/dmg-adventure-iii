f34597c8b9365e183f11e49ad9d5cda188a146e2
john-lay <john_lay@hotmail.co.uk>
2021-10-07 22:25:02 +0100

    Fix sprite limitation in quest status

--- assets/tilemaps/quest-items.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="quest-items" tilewidth="8" tileheight="8" tilecount="8" columns="4">
- <image source="../tilesets/quest-items.png" width="32" height="16"/>
-</tileset>
